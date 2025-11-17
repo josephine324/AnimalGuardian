6d6245c3703d5ca92b6a4efb26bd8e396706cf34
josephine324 <j.mutesi@alustudent.com>
2025-11-17 20:16:44 +0200

    Fix missing go_router import in create_post_screen.dart

--- frontend/lib/features/community/presentation/screens/create_post_screen.dart
+++ frontend/lib/features/community/presentation/screens/create_post_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../providers/community_provider.dart';
 import '../../../../core/models/community_model.dart';
@@ -86,7 +87,7 @@ class _CreatePostScreenState extends ConsumerState<CreatePostScreen> {
             backgroundColor: Colors.green,
           ),
         );
-        Navigator.of(context).pop();
+        context.pop();
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
