513c8f324767c8d23333968e99dd710022487fe4
Josephine Mutesi <127325233+josephine324@users.noreply.github.com>
2025-11-18 19:56:17 +0200

    Add root API endpoint to provide API information - Create api_root view that returns API documentation - Fix 404 error when accessing root URL - Provides helpful information about available endpoints

--- backend/animalguardian/urls.py
+++ backend/animalguardian/urls.py
@@ -5,8 +5,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import api_root
 
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/livestock/', include('livestock.urls')),
